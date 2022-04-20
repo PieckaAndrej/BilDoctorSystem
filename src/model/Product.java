0c1d09e5708e6507096bc4090d494a7cb7ddeff6
CosminRaita <93519047+CosminRaita@users.noreply.github.com>
2022-04-20 12:43:33 +0200

    Model and controller creation

---
+++ src/model/Product.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Product {
+	
+	private int currentStock;
+	
+	private double price;
+	
+	public Product(int currentStock, double price)
+	{
+		
+	}
+
+	/**
+	 * @return the currentStock
+	 */
+	public int getCurrentStock() {
+		return currentStock;
+	}
+
+	/**
+	 * @param currentStock the currentStock to set
+	 */
+	public void setCurrentStock(int currentStock) {
+		this.currentStock = currentStock;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+	
+}
