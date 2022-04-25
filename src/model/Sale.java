0a7d34086e3b84a2337cce33ccd1b6891c4713d4
BarnabyUCN <10407536@ucn.dk>
2022-04-25 09:46:40 +0200

    sale model

--- src/model/Sale.java
+++ src/model/Sale.java
@@ -1,19 +1,21 @@
 package model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 //Barnabas doing this do not touch
 public class Sale {
 	private String description;
 	private LocalDateTime date;
-	private List<OrderLine> orderlines;
+	private ArrayList<OrderLine> orderlines;
+	private Vehicle vehicle;
 	
 	/**
 	 * Constructor for the Sale class
 	 * @param Vehicle
 	 */
-	public Sale(vehicle Vehicle) {
-		orderLines = ArrayList<>();
+	public Sale(Vehicle vehicle) {
+		orderlines = new ArrayList<>();
 		this.vehicle = vehicle;
 	}
 	/**
@@ -21,15 +23,15 @@ public class Sale {
 	 * @param Service
 	 * @return
 	 */
-	public boolean addService(service Service) {
-		
+	public boolean addService(Service service) {
+		return true;
 	}
 	/**
 	 * Add orderline to the sale
 	 * @param OrderLine
 	 * @return
 	 */
-	public boolean addOrderLine(orderLine OrderLine) {
-		return orderLines.add(orderLine);
+	public boolean addOrderLine(OrderLine orderline) {
+		return orderlines.add(orderline);
 	}
 }
