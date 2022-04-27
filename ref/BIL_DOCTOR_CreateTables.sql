370fdff96a44c38392205efb3aaab2ad95255aab
CosminRaita <93519047+CosminRaita@users.noreply.github.com>
2022-04-27 12:08:59 +0200

    Database changes

--- ref/BIL_DOCTOR_CreateTables.sql
+++ ref/BIL_DOCTOR_CreateTables.sql
@@ -4,21 +4,19 @@ USE [CSC-CSD-S211_10407543]
 ALTER TABLE dbo.Person
 DROP CONSTRAINT if exists PersonCityFK;
 ALTER TABLE dbo.Employee
-DROP CONSTRAINT if exists EmployeePersonFK
+DROP CONSTRAINT if exists EmployeePersonFK;
 ALTER TABLE dbo.Customer
 DROP CONSTRAINT if exists CustomerPersonFK;
-ALTER TABLE dbo.Phone
-DROP CONSTRAINT if exists PhonePersonFK, PhoneCityFK;
 ALTER TABLE dbo.Vehicle
 DROP CONSTRAINT if exists VehicleCustomerFK;
 ALTER TABLE dbo.Sale
 DROP CONSTRAINT if exists SaleVehicleFK;
 ALTER TABLE dbo.[Service]
-DROP CONSTRAINT if exists ServiceSaleDateFK, ServiceSalePlateFK;
+DROP CONSTRAINT if exists ServiceSaleFK;
 ALTER TABLE dbo.OrderLine
-DROP CONSTRAINT if exists OrderLineSaleDateFK, OrderLineSalePlateFK, OrderLineProductFK;
+DROP CONSTRAINT if exists OrderLineSaleFK, OrderLineProductFK;
 ALTER TABLE dbo.Appointment
-DROP CONSTRAINT if exists AppointmentSalePlateFK, AppointmentSaleDateFK, AppointmentEmployeeFK, AppointmentCustomerFK;
+DROP CONSTRAINT if exists AppointmentSaleFK, AppointmentEmployeeFK;
 
 GO
 
@@ -26,7 +24,6 @@ drop table if exists dbo.City;
 drop table if exists dbo.Person;
 drop table if exists dbo.Employee;
 drop table if exists dbo.Customer;
-drop table if exists dbo.Phone;
 drop table if exists dbo.Vehicle;
 drop table if exists dbo.Sale;
 drop table if exists dbo.[Service];
@@ -46,15 +43,16 @@ CREATE TABLE dbo.City (
 GO
 
 
-CREATE TABLE dbo.Person (
-	id int PRIMARY KEY IDENTITY(1,1),
+CREATE TABLE dbo.Person ( 
 	[name] VARCHAR(25) NOT NULL,
 	surname VARCHAR(25) NOT NULL,
 	zipcode VARCHAR(5),
 	[address] VARCHAR(50) NOT NULL,
 	association VARCHAR(1) NOT NULL,
+	countryCode VARCHAR(5),
+	phoneNumber VARCHAR(20) PRIMARY KEY,
 	CONSTRAINT PersonCityFK
-		FOREIGN KEY (zipcode) REFERENCES City(zipcode)
+		FOREIGN KEY (countryCode, zipcode) REFERENCES City(countryCode, zipcode)
 		ON DELETE SET NULL,
 	)  
 
@@ -62,37 +60,21 @@ GO
 
 
 CREATE TABLE dbo.Employee (
-   id int PRIMARY KEY IDENTITY(1,1),  
    salary MONEY NOT NULL,
+   phoneNo VARCHAR(20) PRIMARY KEY,
    CONSTRAINT EmployeePersonFK
-		FOREIGN KEY (id) REFERENCES Person(id)
-		ON DELETE SET NULL,
+		FOREIGN KEY (phoneNo) REFERENCES Person(phoneNumber)
+		ON DELETE CASCADE,
 	)  
 
 GO
 
 CREATE TABLE dbo.Customer (
-   id int PRIMARY KEY IDENTITY(1,1),  
    discount decimal(5,5) NOT NULL,
+   phoneNo VARCHAR(20) PRIMARY KEY,
    CONSTRAINT CustomerPersonFK
-		FOREIGN KEY (id) REFERENCES Person(id)
-		ON DELETE SET NULL,
-	)  
-
-GO
-
-
-CREATE TABLE dbo.Phone (
-	phoneNumber VARCHAR(20) NOT NULL,
-	countryCode VARCHAR(5) NOT NULL,
-	personId int NOT NULL,
-	PRIMARY KEY (phoneNumber, countryCode),
-	CONSTRAINT PhonePersonFK
-		FOREIGN KEY (personId) REFERENCES Person(id)
-		ON DELETE SET NULL,
-	CONSTRAINT PhoneCityFK
-		FOREIGN KEY (countryCode) REFERENCES City(countryCode)
-		ON DELETE SET NULL,
+		FOREIGN KEY (phoneNo) REFERENCES Person(phoneNumber)
+		ON DELETE CASCADE,
 	)  
 
 GO
@@ -101,9 +83,9 @@ CREATE TABLE dbo.Vehicle (
 	plateNumber VARCHAR(15) PRIMARY KEY NOT NULL,
 	[year] int NOT NULL,
 	brand VARCHAR(20) NOT NULL,
-	customerId int NOT NULL,
+	customerPhone VARCHAR(20),
 	CONSTRAINT VehicleCustomerFK
-		FOREIGN KEY (customerId) REFERENCES Customer(id)
+		FOREIGN KEY (customerPhone) REFERENCES Customer(phoneNo)
 		ON DELETE SET NULL,
 	)  
 
@@ -111,13 +93,13 @@ GO
 
 
 CREATE TABLE dbo.Sale (
-	plateNumber VARCHAR(15) NOT NULL,
+	plateNo VARCHAR(15) NOT NULL,
 	[date] datetime NOT NULL, 
 	[description] VARCHAR(500) NOT NULL,
-	PRIMARY KEY (plateNumber, [date]),
+	PRIMARY KEY (plateNo, [date]),
 	CONSTRAINT SaleVehicleFK
-		FOREIGN KEY (plateNumber) REFERENCES Vehicle(plateNumber)
-		ON DELETE SET NULL,
+		FOREIGN KEY (plateNo) REFERENCES Vehicle(plateNumber)
+		ON DELETE CASCADE,
 	)  
 
 GO
@@ -127,14 +109,11 @@ CREATE TABLE dbo.[Service] (
 	[description] VARCHAR(500) NOT NULL,
 	[time] decimal(5,2) NOT NULL,
 	price MONEY NOT NULL,
-	plateNumber VARCHAR(15) NOT NULL,
+	plateNr VARCHAR(15) NOT NULL,
 	[date] datetime NOT NULL,
-	CONSTRAINT ServiceSaleDateFK
-		FOREIGN KEY ([date]) REFERENCES Sale([date])
-		ON DELETE SET NULL,
-	CONSTRAINT ServiceSalePlateFK
-		FOREIGN KEY (plateNumber) REFERENCES Sale(plateNumber)
-		ON DELETE SET NULL,
+	CONSTRAINT ServiceSaleFK
+		FOREIGN KEY (plateNr, [date]) REFERENCES Sale(plateNo, [date])
+		ON DELETE CASCADE,
 	)  
 
 GO
@@ -150,14 +129,11 @@ GO
 CREATE TABLE dbo.OrderLine (
 	id int PRIMARY KEY IDENTITY(1,1), 
 	quantity int NOT NULL,
-	plateNumber VARCHAR(15) NOT NULL,
+	plateNr VARCHAR(15) NOT NULL,
 	[date] datetime NOT NULL,
 	productId int,
-	CONSTRAINT OrderLineSaleDateFK
-		FOREIGN KEY ([date]) REFERENCES Sale([date])
-		ON DELETE CASCADE,
-	CONSTRAINT OrderLineSalePlateFK
-		FOREIGN KEY (plateNumber) REFERENCES Sale(plateNumber)
+	CONSTRAINT OrderLineSaleFK
+		FOREIGN KEY (plateNr, [date]) REFERENCES Sale(plateNo, [date])
 		ON DELETE CASCADE,
 	CONSTRAINT OrderLineProductFK
 		FOREIGN KEY (productId) REFERENCES Product(id)
@@ -173,22 +149,15 @@ CREATE TABLE dbo.Appointment (
 	creationDate datetime NOT NULL,
 	[date] datetime NOT NULL,
 	[length] decimal(5,2) NOT NULL,
-	plateNumber VARCHAR(15),
+	plateNr VARCHAR(15),
 	saleDate datetime,
-	employeeId int,
-	customerId int,
-	CONSTRAINT AppointmentSalePlateFK
-		FOREIGN KEY (plateNumber) REFERENCES Sale(plateNumber)
-		ON DELETE SET NULL,
-	CONSTRAINT AppointmentSaleDateFK
-		FOREIGN KEY (saleDate) REFERENCES Sale([date])
-		ON DELETE SET NULL,
+	employeePhoneNo VARCHAR(20),
+	CONSTRAINT AppointmentSaleFK
+		FOREIGN KEY (plateNr, [date]) REFERENCES Sale(plateNo, [date])
+		ON DELETE CASCADE,
 	CONSTRAINT AppointmentEmployeeFK
-		FOREIGN KEY (employeeId) REFERENCES Employee(id)
-		ON DELETE SET NULL,
-	CONSTRAINT AppointmentCustomerFK
-		FOREIGN KEY (customerId) REFERENCES Customer(id)
-		ON DELETE SET NULL,
+		FOREIGN KEY (employeePhoneNo) REFERENCES Employee(phoneNo)
+		ON DELETE CASCADE,
 	)
 
 GO
