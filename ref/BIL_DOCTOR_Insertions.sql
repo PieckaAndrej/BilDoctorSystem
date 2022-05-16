c540bb05b7fb968c9ecddf1ac6e3f3a4b361bab7
BarnabyUCN <10407536@ucn.dk>
2022-05-16 11:53:09 +0200

    Update BIL_DOCTOR_Insertions.sql
    easy

--- ref/BIL_DOCTOR_Insertions.sql
+++ ref/BIL_DOCTOR_Insertions.sql
@@ -9,7 +9,7 @@ insert into Customer values ('0', '1');
 
 insert into Vehicle values ('AAA', 1, 'Tesla', '1');
 
-insert into Appointment values ('1', '2022-05-14 15:03:42', '0.5', '2022-05-16 10:00:00', 'The car was involved in an accident and now it makes weird noises', '97845625')
+insert into Appointment values ('1', '2022-05-14 15:03:42', '0.5', '2022-05-16 10:00:00', 'The car was involved in an accident and now it makes weird noises', '97845625');
 
 insert into Product values ('oil', 101231, 100.0);
 insert into Product values ('tire', 5, 400);
